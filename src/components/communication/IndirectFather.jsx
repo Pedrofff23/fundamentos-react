570b05f86350024a53225416463a5546395eb1e1
Pedro Santana Mendes <pmendes09@hotmail.com>
2022-09-17 22:16:30 -0300

    Class 35 Component with State

--- src/components/communication/IndirectFather.jsx
+++ src/components/communication/IndirectFather.jsx
@@ -1,13 +1,26 @@
 import IndirectSon from "./IndirectSon";
+import React, { useState } from "react";
 
-function sendInfo(name, age, nerd) {
-  console.log(name, age, nerd);
-}
-
+// eslint-disable-next-line
 export default (props) => {
+  const [name, setName] = useState("?");
+  const [age, setAge] = useState("0");
+  const [nerd, setNerd] = useState("false");
+
+  function sendInfo(name, age, nerd) {
+    setAge(age);
+    setName(name);
+    setNerd(nerd);
+  }
   return (
     <div>
-      <div>Pai</div>
+      <div>
+        <span>{name} </span>
+        <span>
+          <strong>{age} </strong>
+        </span>
+        <span>{nerd ? "True" : "False"}</span>
+      </div>
       <IndirectSon clicked={sendInfo}></IndirectSon>
     </div>
   );
--- src/components/communication/IndirectSon.jsx
+++ src/components/communication/IndirectSon.jsx
@@ -1,11 +1,14 @@
+
+// eslint-disable-next-line
 export default (props) => {
-    const cb = props.clicked
+  const cb = props.clicked;
+  const randomAge = () => parseInt(Math.random() * (20)) + 50
+  const randomNerd = () => Math.random() > 0.5
+
   return (
     <div>
       <div>Son</div>
-      <button onClick={_ => cb("Joao", 54, false)}>
-        Send info
-      </button>
+      <button onClick={(_) => cb("Joao", randomAge(), randomNerd())}>Send info</button>
     </div>
   );
 };
